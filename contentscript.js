79977285a8432c3704d5f10db26fb45bf8e57039
tamanishi <tamanishi@gmail.com>
2015-03-20 15:59:08 +0900

    It Works!

--- contentscript.js
+++ contentscript.js
@@ -1,6 +1,21 @@
-chrome.extension.onMessage.addListener ( function(
-    request, sender, sendResponse) {
+function onMessageHandler(message, sender, sendResponse) {
     console.log("received.");
-    alert("recieved.");
-    sendResponse({});
-});
+    var result = "";
+    var count = 0;
+    var query = window.getSelection().toString();
+    if (query.length > 0) {
+        console.log(window.getSelection().toString());
+        $(":not(:has(*))").each( function (index, element) {
+            if (~$(element).text().indexOf(query)) {
+                // console.log($(element).html());
+                result += $(element).get(0).outerHTML;
+                result += "\n--------\n";
+                count++;
+            }
+        });
+    }
+
+    sendResponse({result: result, count: count});
+};
+
+chrome.runtime.onMessage.addListener(onMessageHandler);
--- main.js
+++ main.js
@@ -1,7 +1,23 @@
 function onClickHandler(info, tab) {
     console.log(info.menuItemId + " clicked.");
-    chrome.extension.sendMessage({action : "none"}, function(response) {
+    chrome.tabs.sendMessage(tab.id, {query : "Google"}, function(response) {
         console.log("responded.");
+        // console.log(response.result);
+
+        if(response.count > 0) {
+            var textArea = document.createElement("textarea");
+            textArea.style.cssText = "position:absolute;left:-100%";
+
+            document.body.appendChild(textArea);
+
+            textArea.value = response.result;
+            textArea.select();
+            document.execCommand("copy");
+
+            document.body.removeChild(textArea);
+        }
+
+        alert(response.count + " element(s) copied!");
     });
 };
 
@@ -9,8 +25,9 @@ chrome.contextMenus.onClicked.addListener(onClickHandler);
 
 chrome.runtime.onInstalled.addListener(function() {
     chrome.contextMenus.create({
-        "title" : "Search",
-        "id" : "search_id"
+        "title": "Search Selected and Copy Them.",
+        "id": "search_id",
+        "contexts": ["selection"]
     });
     console.log("menu created.");
 
